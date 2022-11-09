83afcd4d5860a2c095c6687938576853bb16149a
JeanSimon20 <jean.simon@vallegrande.edu.pe>
2022-11-09 08:57:51 -0500

    Implementacion del Properties

---
+++ src/main/java/com/jsimon/compra/model/Compra.java
@@ -0,0 +1,14 @@
+package com.jsimon.compra.model;
+
+import lombok.Data;
+
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Data
+@Table(name = "compra")
+public class Compra implements Serializable {
+
+
+
+}
