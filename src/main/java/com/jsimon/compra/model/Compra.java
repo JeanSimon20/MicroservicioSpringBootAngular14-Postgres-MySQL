a329b61b554389d8834e07e8d7015343f281d257
JeanSimon20 <jean.simon@vallegrande.edu.pe>
2022-11-09 09:06:16 -0500

    Implementacion del Modelo con el Sonarqube

--- src/main/java/com/jsimon/compra/model/Compra.java
+++ src/main/java/com/jsimon/compra/model/Compra.java
@@ -2,13 +2,32 @@ package com.jsimon.compra.model;
 
 import lombok.Data;
 
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
+@Entity
 @Data
 @Table(name = "compra")
 public class Compra implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "inmueble_id", nullable = false)
+    private Long inmuebleId;
+
+    @Column(name = "titulo", nullable = false)
+    private String titulo;
+
+    @Column(name = "precio", nullable = false)
+    private Double precio;
+
+    @Column(name = "fecha_compra",nullable = false)
+    private LocalDateTime fecha_compra;
 
 }
